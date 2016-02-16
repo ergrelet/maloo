e64145e97792c2db9d4cd091016ce60fe8bf8fab
ergrelet <grelet.erwan@gmail.com>
2016-02-16 17:00:55 +0100

    Formatting

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import sys
 import ircbot
 
 def main():
-	print("Maloo Bot - In Python !\n")
+    print("Maloo Bot - In Python !\n")
 
-	if len(sys.argv) < 2:
-		sys.exit("Missing argument (config file) !")
+    if len(sys.argv) < 2:
+        sys.exit("Missing argument (config file) !")
 
-	config = configparser.ConfigParser()
-	config.read(sys.argv[1])
-	bot = ircbot.IrcBot(config)
-	bot.start()
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
+    bot = ircbot.IrcBot(config)
+    bot.start()
 
 if __name__ == "__main__":
-	main()
+    main()
